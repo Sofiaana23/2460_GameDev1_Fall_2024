2c44511290aa3506810a94ce567f89396afe163d
Ana Eyre <annpasc93oc@gmail.com>
2024-08-27 00:00:10 -0600

    deleted mono behavior

--- Scriptable object lab/Assets/Scripts/FloatData.cs
+++ Scriptable object lab/Assets/Scripts/FloatData.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[CreateAssetMenu]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class NewBehaviourScript : ScriptableObject
+{
+    public float value;
 }
