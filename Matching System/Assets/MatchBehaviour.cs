79c68835c3c4f7914c14f033a8854883bcc0d981
Ana Eyre <annpasc93oc@gmail.com>
2024-09-09 22:30:21 -0600

    No matching objects

---
+++ Matching System/Assets/MatchBehaviour.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class MatchBehaviour : MonoBehaviour
+{
+    public ID idObj;
+
+    private void OnTriggerEnter (Collider other) 
+    {
+        var tempObj = other.GetComponent<IDContainerBehaviour>();
+        if (tempObj == null)
+            return;
+
+        var otherID = tempObj.idObj;
+        if (otherID == idObj)
+
+        {
+            Debug.Log("Matched");
+        }
+        else
+        {
+            Debug.Log("No Match");
+        }
+
+    }
+}
