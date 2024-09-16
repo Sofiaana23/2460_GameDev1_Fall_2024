a7be2af3261c3ec0f848e206d20449865b7f5f1a
Ana Eyre <annpasc93oc@gmail.com>
2024-09-16 00:20:06 -0600

    ScoreNum scripts

--- Matching System/Assets/IntData.cs
+++ Matching System/Assets/IntData.cs
@@ -1,31 +1,41 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 [CreateAssetMenu]
-public class IntData :  ScriptableObject
+public class IntData : ScriptableObject
 {
     public int value;
 
-    public UnityEvent disableEvent;
-    
-   
     public void SetValue(int num) 
     {
         value = num;
     }
 
-    public void SetValue (IntData obj)
+    public void CompareValue(IntData obj) 
+    {
+        if (value >= obj.value) 
+        {
+            
+        }
+        else
+        {
+            value = obj.value;
+        }
+        
+    }
+
+    public void SetValue(IntData obj) 
     {
         value = obj.value;
     }
 
-    public void UpdateValue (int num) 
+    public void UpdateValue(int num) 
     {
         value += num;
     }
 
-    private void OnDisable()
-    {
-        disableEvent.Invoke();
-    }
+    
+
 }
+
