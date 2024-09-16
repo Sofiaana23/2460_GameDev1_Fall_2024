b16b162e9b1606c3df2efef096a70b7976e94130
Ana Eyre <annpasc93oc@gmail.com>
2024-09-15 19:53:59 -0600

    Attempted to fix Merge Conflict
    Previous commit included a scene file with an unresolved merge conflict.  This push attempts to commit some of the homework and fix the broken scene file

--- Matching System/Assets/IntData.cs
+++ Matching System/Assets/IntData.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 [CreateAssetMenu]
 public class IntData :  ScriptableObject
 {
     public int value;
 
+    public UnityEvent disableEvent;
+    
+   
     public void SetValue(int num) 
     {
         value = num;
     }
 
+    public void SetValue (IntData obj)
+    {
+        value = obj.value;
+    }
+
     public void UpdateValue (int num) 
     {
         value += num;
+    }
+
+    private void OnDisable()
+    {
+        disableEvent.Invoke();
     }
 }
