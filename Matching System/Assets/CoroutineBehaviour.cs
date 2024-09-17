8994efd19cab7d778eabd37cb5b47df69bb7e0a0
Ana Eyre <annpasc93oc@gmail.com>
2024-09-17 15:45:39 -0600

    On and off integer animation

--- Matching System/Assets/CoroutineBehaviour.cs
+++ Matching System/Assets/CoroutineBehaviour.cs
@@ -1,27 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CoroutineBehaviour : MonoBehaviour
 {
-    public bool canRun = true;
-    // Start is called before the first frame update
+    public UnityEvent repeatEvent;
+    
+    public int counterNum = 3;
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj;
+    private WaitForFixedUpdate wffuObj;
+    
     IEnumerator Start()
     {
-        Debug.Log("Start");
-        yield return new WaitForSeconds(3);
-        Debug.Log("Late Start");
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
 
-        while (canRun) 
+        while (counterNum > 0) 
         {
-            yield return new WaitForFixedUpdate();
-            Debug.Log("Run on Start");
+            yield return wfsObj;
+            repeatEvent.Invoke();
         }
+        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Run");
-    }
+    
 }
