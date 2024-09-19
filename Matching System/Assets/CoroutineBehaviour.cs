e7d1aee981a40d977ec2690ffa4f34687286edd3
Ana Eyre <annpasc93oc@gmail.com>
2024-09-19 00:26:29 -0600

    coroutine behavior fixed in 321go scene

--- Matching System/Assets/CoroutineBehaviour.cs
+++ Matching System/Assets/CoroutineBehaviour.cs
@@ -5,32 +5,47 @@ using UnityEngine.Events;
 
 public class CoroutineBehaviour : MonoBehaviour
 {
-    public UnityEvent startEvent, repeatEvent, endEvent, repeatUntilFalseEvent;
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
 
     public bool canRun;
     public IntData counterNum;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    public void StartCounting()
+    {
+        StartCoroutine(routine: Counting());
+    }
     private IEnumerator Counting()
     {
         wfsObj = new WaitForSeconds(seconds);
         wffuObj = new WaitForFixedUpdate();
 
-        startEvent.Invoke();
+        startCountEvent.Invoke();
         while (counterNum.value > 0) 
         {
             Debug.Log(counterNum);
-            repeatEvent.Invoke();
+            repeatCountEvent.Invoke();
             counterNum.value--;
             yield return wfsObj;
             
            
         }
-        endEvent.Invoke();
+        endCountEvent.Invoke();
         
     }
 
+    public void StartRepeatUntilFalse() 
+    {
+        canRun = true;
+        StartCoroutine(routine: RepeatUntilFalse());
+    }
     private IEnumerator RepeatUntilFalse() 
     {
         while (canRun) 
