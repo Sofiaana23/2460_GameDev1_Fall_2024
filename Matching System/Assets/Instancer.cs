95b1ef41bf65643d7e36f3876bc40d4d694f6b8c
Ana Eyre <annpasc93oc@gmail.com>
2024-09-23 16:48:13 -0600

    Vector3 instantiate

--- Matching System/Assets/Instancer.cs
+++ Matching System/Assets/Instancer.cs
@@ -1,13 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
 
 public class Instancer : ScriptableObject
 {
-    public void CreateInstance(GameObject obj) 
+    public GameObject prefab;
+    public void CreateInstance()
     {
-        Instantiate(obj);
+        Instantiate(prefab);
     }
+
+    public void CreateInstance (Vector3Data obj) 
+    {
+        Instantiate(prefab, obj.value, Quaternion.identity);
+    }
+
 }
